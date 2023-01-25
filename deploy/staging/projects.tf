847b0eae8c0f1eab07f3b405ffbfd8a7aa799f5a
Lucas Slebos <lucas@slebos.nz>
2023-01-25 18:45:47 +1300

    update project description to remove trailing period

--- deploy/staging/projects.tf
+++ deploy/staging/projects.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_project" "environment" {
   name        = title(var.environment)
-  description = "${title(var.environment)} environment for applications."
+  description = "${title(var.environment)} environment for applications"
   purpose     = "Web Application"
   environment = title(var.environment)
   is_default  = false
