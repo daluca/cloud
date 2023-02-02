f60ab73c24308735bf25be0758669c916781fbce
Lucas Slebos <lucas@slebos.nz>
2023-02-03 11:21:41 +1300

    update domain and ip addresses to be sensitive

--- deploy/staging/loadbalancers.tf
+++ deploy/staging/loadbalancers.tf
@@ -1,3 +1,7 @@
+locals {
+  loadbalancer_ip_address = sensitive(data.digitalocean_loadbalancer.staging.ip)
+}
+
 data "digitalocean_loadbalancer" "staging" {
   name = lower(digitalocean_project.staging.name)
 }
