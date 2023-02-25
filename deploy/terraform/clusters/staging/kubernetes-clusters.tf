2117a4c55feecffb5459aa6386e2ed8c0e74f8cc
Lucas Slebos <lucas@slebos.nz>
2023-02-25 14:34:49 +1300

    update worker node slug to increase resources

--- deploy/terraform/clusters/staging/kubernetes-clusters.tf
+++ deploy/terraform/clusters/staging/kubernetes-clusters.tf
@@ -13,7 +13,7 @@ resource "digitalocean_kubernetes_cluster" "staging" {
 
   node_pool {
     name       = "worker-pool"
-    size       = "s-2vcpu-4gb"
+    size       = "s-4vcpu-8gb"
     node_count = var.kubernetes_node_count
   }
 }
