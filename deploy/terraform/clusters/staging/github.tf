ae8ccb430b701e7e93da7f40c9b5dbc28ff12dd4
Lucas Slebos <lucas@slebos.nz>
2023-02-27 17:43:31 +1300

    update file name to better reflect resources

--- deploy/terraform/clusters/staging/github.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  github_known_hosts = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
-}
