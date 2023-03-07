476b5685d6df57eb2fac2629d132e9acc4ee9b73
Lucas Slebos <lucas@slebos.nz>
2023-03-07 16:05:24 +1300

    add Catalyst Cloud container backend

--- deploy/terraform/accounts/digitalocean/providers.tf
+++ deploy/terraform/accounts/digitalocean/providers.tf
@@ -7,4 +7,14 @@ terraform {
       version = "~> 2.0"
     }
   }
+
+  backend "s3" {
+    endpoint = "object-storage.nz-wlg-2.catalystcloud.io"
+    key      = "accounts/digitalocean/terraform.tfstate"
+    region   = "us-east-1" # Must be an AWS region
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
 }
