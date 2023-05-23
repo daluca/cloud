bcc752ff5564bd788a608b361a9688d8cffcdf51
Lucas Slebos <lucas@slebos.nz>
2023-05-23 15:05:13 +1200

    add cloudflare api token for cert-manager from terraform

--- deploy/terraform/modules/environment/kubernetes-secrets.tf
+++ deploy/terraform/modules/environment/kubernetes-secrets.tf
@@ -10,3 +10,16 @@ resource "kubernetes_secret" "sops" {
     "cluster.agekey" = var.flux.age_key
   }
 }
+
+resource "kubernetes_secret" "cert_manager_cloudflare_api_token" {
+  depends_on = [module.fluxcd]
+
+  metadata {
+    name      = "cloudflare-api-token"
+    namespace = "cert-manager"
+  }
+
+  data = {
+    token = cloudflare_api_token.cert_manager.value
+  }
+}
