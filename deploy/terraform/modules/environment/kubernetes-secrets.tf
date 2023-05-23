e0e6e0575ebad34449c44d6703a0f258cc0be3ac
Lucas Slebos <lucas@slebos.nz>
2023-05-23 15:07:55 +1200

    add cloudflare api token for external-dns from terraform

--- deploy/terraform/modules/environment/kubernetes-secrets.tf
+++ deploy/terraform/modules/environment/kubernetes-secrets.tf
@@ -23,3 +23,16 @@ resource "kubernetes_secret" "cert_manager_cloudflare_api_token" {
     token = cloudflare_api_token.cert_manager.value
   }
 }
+
+resource "kubernetes_secret" "extrenal_dns_cloudflare_api_token" {
+  depends_on = [module.fluxcd]
+
+  metadata {
+    name      = "cloudflare-credentials"
+    namespace = "kube-system"
+  }
+
+  data = {
+    token = cloudflare_api_token.external_dns.value
+  }
+}
