c0fbc8149af8a4f034f019e335150ddd2fd1454e
Lucas Slebos <lucas@slebos.nz>
2023-08-26 13:07:11 +1200

    add OIDC to miniflux

--- deploy/terraform/modules/keycloak/kubernetes-secrets.tf
+++ deploy/terraform/modules/keycloak/kubernetes-secrets.tf
@@ -9,3 +9,15 @@ resource "kubernetes_secret" "nextcloud_openid_connect" {
     "client-secret" = keycloak_openid_client.nextcloud.client_secret
   }
 }
+
+resource "kubernetes_secret" "miniflux_openid_connect" {
+  metadata {
+    name      = "miniflux-openid-connect"
+    namespace = "miniflux"
+  }
+
+  data = {
+    "OAUTH2_CLIENT_ID"     = keycloak_openid_client.miniflux.client_id
+    "OAUTH2_CLIENT_SECRET" = keycloak_openid_client.miniflux.client_secret
+  }
+}
