e83cb707afa1f188d4aa3db462a383c903256da4
Lucas Slebos <lucas@slebos.nz>
2023-11-28 13:59:13 +1300

    add OIDC groups client scope to oauth2-proxy

---
+++ deploy/terraform/modules/keycloak/keycloak-openid-client-scopes.tf
@@ -0,0 +1,25 @@
+resource "keycloak_openid_client_scope" "group" {
+  realm_id = data.keycloak_realm.main.id
+
+  name                   = "groups"
+  description            = "OpenID Connect scope for mapping a user's group membership to a claim"
+  include_in_token_scope = true
+}
+
+resource "keycloak_generic_protocol_mapper" "groups_mapper" {
+  realm_id        = data.keycloak_realm.main.id
+  client_scope_id = keycloak_openid_client_scope.group.id
+
+  name            = "groups-mapper"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-group-membership-mapper"
+
+  config = {
+    "claim.name" = "groups"
+    "full.path" : "false",
+    "id.token.claim" : "true",
+    "access.token.claim" : "true",
+    "userinfo.token.claim" : "true"
+    "multivalued" : "true"
+  }
+}
