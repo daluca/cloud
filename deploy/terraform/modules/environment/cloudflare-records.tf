2acd864f05c232d252405add272a2011689f78d0
Lucas Slebos <lucas@slebos.nz>
2023-12-01 13:20:56 +1300

    add SRV record to matrix domain

--- deploy/terraform/modules/environment/cloudflare-records.tf
+++ deploy/terraform/modules/environment/cloudflare-records.tf
@@ -1,3 +1,7 @@
+locals {
+  matrix_domain = local.environment == "staging" ? var.cloudflare.domains[0] : var.cloudflare.domains[1]
+}
+
 resource "cloudflare_record" "apex" {
   for_each = toset(var.cloudflare.domains)
 
@@ -5,5 +9,21 @@ resource "cloudflare_record" "apex" {
   name    = "@"
   value   = sensitive(data.digitalocean_loadbalancer.main.ip)
   type    = "A"
-  proxied = true
+  proxied = each.key != local.matrix_domain ? true : false
+}
+
+resource "cloudflare_record" "matrix_srv" {
+  zone_id = sensitive(data.cloudflare_zone.domains[local.matrix_domain].id)
+  name    = "@"
+  type    = "SRV"
+
+  data {
+    service  = "_matrix"
+    proto    = "_tcp"
+    name     = data.cloudflare_zone.domains[local.matrix_domain].name
+    priority = 10
+    weight   = 0
+    port     = 443
+    target   = "matrix.${data.cloudflare_zone.domains[local.matrix_domain].name}"
+  }
 }
