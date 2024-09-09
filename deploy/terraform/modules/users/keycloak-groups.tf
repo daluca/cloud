ba054f46cdf620b666774d7a270da3109d37b3ed
Lucas Slebos <lucas@slebos.nz>
2024-09-09 22:26:41 +1200

    add new groups to users module

--- deploy/terraform/modules/users/keycloak-groups.tf
+++ deploy/terraform/modules/users/keycloak-groups.tf
@@ -2,6 +2,8 @@ locals {
   groups = {
     (data.keycloak_group.administrators.path) = data.keycloak_group.administrators
     (data.keycloak_group.users.path)          = data.keycloak_group.users
+    (data.keycloak_group.mealie_admin.path)   = data.keycloak_group.mealie_admin
+    (data.keycloak_group.mealie_user.path)    = data.keycloak_group.mealie_user
   }
 }
 
@@ -13,4 +15,14 @@ data "keycloak_group" "administrators" {
 data "keycloak_group" "users" {
   realm_id = data.keycloak_realm.environment.id
   name     = "Users"
+}
+
+data "keycloak_group" "mealie_admin" {
+  realm_id = data.keycloak_realm.environment.id
+  name     = "MealieAdmin"
+}
+
+data "keycloak_group" "mealie_user" {
+  realm_id = data.keycloak_realm.environment.id
+  name     = "MealieUser"
 }
